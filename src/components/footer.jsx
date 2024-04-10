756fad3721953baa4e0a5b1a005a32f9229b518c
millerladino <millerladino@gmail.com>
2024-04-09 20:15:55 -0500

    subida del proyecto de React

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer(){
+
+    return (
+        <div className="bg-blue-800 text-white py-4 text-center  bottom-0 w-full">
+            <h1>Proyecto talento Tech</h1>
+            <h2 className="text-red-500">Desarrollador por Miller Ladino</h2>
+        </div>
+    );
+}
